ff7b5ac7a65caddcb621cd606768fff30508583a
IanC13 <ian.yl.cheng@gmail.com>
2025-03-04 15:54:54 -0500

    Add black, flake8 and coverage

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,13 @@
 from django.db.models import F
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
 
 # Create your views here.
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
@@ -16,9 +17,11 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by("-pub_date")[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
 
 class ResultsView(generic.DetailView):
     model = Question
